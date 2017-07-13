c3f64a7639a95beb45a462b3eb9c264ca077b5d1
Wayne Burns <woburns@gmail.com>
2017-07-13 12:36:40 -0400

    initial commit

---
+++ public/app.js
@@ -0,0 +1,42 @@
+// public/core.js
+var meanTodo = angular.module('meanTodo', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('https://polar-scrubland-51717.herokuapp.com/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('https://polar-scrubland-51717.herokuapp.com/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('https://polar-scrubland-51717.herokuapp.com/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
